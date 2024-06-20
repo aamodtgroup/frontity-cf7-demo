4d3aa7d726a23ae3dca0bacc978c1aca4ff430a9
Kasper Aamodt <kasper@aamodt.xyz>
2024-06-20 11:17:30 +0200

    update wp url

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,52 +1,49 @@
 const settings = {
-  "name": "frontity-cf7-demo",
-  "state": {
-    "frontity": {
-      "url": "https://frontity-cf7.aamodtgroup.com",
-      "title": "Frontity CF7 Demo",
-      "description": "This is a Frontity CF7 demo"
-    }
+  name: "frontity-cf7-demo",
+  state: {
+    frontity: {
+      url: "https://frontity-cf7.devhuset.dev",
+      title: "Frontity CF7 Demo",
+      description: "This is a Frontity CF7 demo",
+    },
   },
-  "packages": [
+  packages: [
     {
-      "name": "@frontity/mars-theme",
-      "state": {
-        "theme": {
-          "menu": [
+      name: "@frontity/mars-theme",
+      state: {
+        theme: {
+          menu: [
             [
               "GitHub",
-              "https://github.com/aamodtgroup/frontity-contact-form-7"
+              "https://github.com/aamodtgroup/frontity-contact-form-7",
             ],
             [
               "Issues",
-              "https://github.com/aamodtgroup/frontity-contact-form-7/issues"
+              "https://github.com/aamodtgroup/frontity-contact-form-7/issues",
             ],
-            [
-              "Aamodt Group",
-              "https://aamodtgroup.com"
-            ]
+            ["Devhuset", "https://devhuset.no"],
           ],
-          "featured": {
-            "showOnList": false,
-            "showOnPost": false
-          }
-        }
-      }
+          featured: {
+            showOnList: false,
+            showOnPost: false,
+          },
+        },
+      },
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "url": "https://wp-frontity-cf7.aamodtgroup.com",
-          "homepage": "home"
-        }
-      }
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          url: "https://wp-cf7.devhuset.dev",
+          homepage: "home",
+        },
+      },
     },
     "@aamodtgroup/frontity-contact-form-7",
     "@frontity/tiny-router",
     "@frontity/html2react",
-    "@frontity/yoast"
-  ]
+    "@frontity/yoast",
+  ],
 };
 
 export default settings;
